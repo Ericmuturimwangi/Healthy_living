620a9ec3780d2f2ad6dab2f69a822c63d240f3f0
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-08-11 22:45:04 +0300

    create view, and recipe template

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("djrichtextfield/", include("djrichtextfield.urls")),
     path("", include("home.urls")),
+    path("recipes/", include("recipes.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Recipe
+from .forms import RecipeForm
 
-# Create your views here.
+# to check if user is logged in first
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class AddRecipe(LoginRequiredMixin, CreateView):
+    # addimng the frecipe view
+    template_name = "recipes/add_recipe.html"
+    model = Recipe
+    form_class = RecipeForm
+    success_url = "/recipes/"
+
+    def form_valid(self, form):
+        # setting insatnce user to person who is logged in.
+        form.instance.user = self.request.user
+        return super(AddRecipe, self).form_valid(form)
